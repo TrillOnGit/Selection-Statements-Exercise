c6792ded8607a6c1afeb6d0caa90b937fb5b3258
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-21 14:45:45 -0400

    Initial commit, completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1,100);
+            
+            // The following code will allow the user to guess what your favorite number is
+            // (be sure to prompt the user what to input):
+            Console.WriteLine("Guess what my favorite number is (its a number between 1 and 100)!");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("You're too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("That's too high!");
+            }
+            else
+            {
+                Console.WriteLine("That's correct!");
+            }
         }
     }
 }
